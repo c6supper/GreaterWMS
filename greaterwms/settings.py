39bb25342b5e6905b3b5466c74909037d3ee59f5
c6supper <c6supper@hotmail.com>
2021-08-11 20:32:50 +0800

    can bringup in docker

--- greaterwms/settings.py
+++ greaterwms/settings.py
@@ -103,7 +103,7 @@ WSGI_APPLICATION = 'greaterwms.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'test',
         'USER': 'root',
         'PASSWORD': '123',
